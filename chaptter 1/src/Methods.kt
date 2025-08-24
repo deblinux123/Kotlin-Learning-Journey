c3de300184962bc9a97fac60931e7fd581fc7415
farid kaki <deltaburs@gmail.com>
2025-08-24 15:10:12 +0330

    chaptter1

--- chaptter 1/src/Methods.kt
+++ chaptter 1/src/Methods.kt
@@ -1,4 +1,10 @@
 fun main()
 {
     val name = "Farid"
+    println(name.uppercase())
+    println(name.lowercase())
+    println(name.length)
+    println(name.startsWith("f"))
+    println(name.substring(1, 3))
+    println(name.equals("Farid"))
 }
