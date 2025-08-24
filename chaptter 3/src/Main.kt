ff00e4a17c46bc6730b4794ffc0c07223a861ee8
farid kaki <deltaburs@gmail.com>
2025-08-24 16:12:04 +0330

    conditions if else and when

--- chaptter 3/src/Main.kt
+++ chaptter 3/src/Main.kt
@@ -1,14 +1,93 @@
+import kotlin.random.Random
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val d: Int
+    val check: Boolean = true
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    if (check)
+    {
+        d = 1
     }
+    else
+    {
+        d = 2
+    }
+
+    println(d)
+
+    println("Please enter your password: ")
+    val pasword: String = readlnOrNull()!!
+
+    if (pasword.equals("myPass"))
+    {
+        println("Loged in.")
+    }
+    else
+    {
+        println("Wrong Password.")
+    }
+
+    val number = 2
+
+    when(number)
+    {
+        1 -> println("one")
+        2 -> println("two")
+        3 -> println("three")
+        else -> println("invalid one")
+    }
+
+    println("Enter your age: ")
+    val age= readlnOrNull()?.toIntOrNull()
+    when (age)
+    {
+        in 0..12 -> println("Child")
+        in 13..19 -> println("young")
+        in 20..50 -> println("middle age")
+        else -> println("Old")
+    }
+
+    val trafficLightState: String = "red"
+
+    val trafficAction = when
+    {
+        trafficLightState.equals("green") -> "go"
+        trafficLightState.equals("yellow") -> "slow down"
+        trafficLightState.equals("red") -> "stop"
+        else -> "Malfunction"
+    }
+
+    println(trafficAction)
+
+
+    // import kotlin.random.Random
+
+    val firstResult = Random.nextInt(6);
+    val secondResult = Random.nextInt(6);
+
+    if (firstResult.equals(secondResult))
+    {
+        println("You win")
+    }
+    else
+    {
+        println("You lose")
+    }
+
+    val button: Char = 'A'
+
+
+    val result: String = when (button)
+    {
+        'A' -> "yes"
+        'B' -> "No"
+        'X' -> "Menu"
+        'Y' -> "Nothing"
+        else -> "There is no such button"
+    }
+
+    println(result)
+
 }
