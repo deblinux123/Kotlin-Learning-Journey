f1644a3d9b296df38aff98d00c1a67825fb29b7f
farid kaki <deltaburs@gmail.com>
2025-08-25 12:26:57 +0330

    Animal class

---
+++ chaptter 5/src/Animal.kt
@@ -0,0 +1,31 @@
+open class Animal(val name: String, val age: Int)
+{
+    open fun sound()
+    {
+        println("Animal make sound")
+    }
+}
+
+
+class Dog(name: String, age: Int): Animal(name, age)
+{
+    override fun sound() {
+        println("$name make meow meow sound")
+    }
+}
+
+class Cat(name: String, age: Int): Animal(name, age)
+{
+    override fun sound() {
+        super.sound()
+        println("$name make meow meow sound")
+    }
+}
+fun main()
+{
+    val dog = Dog("jack", 2)
+    val cat = Cat("blue", 3)
+
+    dog.sound()
+    cat.sound()
+}
